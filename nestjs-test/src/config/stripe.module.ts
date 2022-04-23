d2fbfce0beed0bcd12eac715d8ebd8a03a5b67af
Jun Guo <gjuoun@gmail.com>
2022-04-23 01:19:42 -0300

    update

---
+++ nestjs-test/src/config/stripe.module.ts
@@ -0,0 +1,23 @@
+import { DynamicModule, Module, Provider } from '@nestjs/common';
+import Stripe from 'stripe';
+
+export const STRIPE_CLIENT = 'STRIPE_CLIENT';
+
+@Module({})
+export class StripeModule {
+  static forRoot(apiKey: string, config: Stripe.StripeConfig): DynamicModule {
+    const stripe = new Stripe(apiKey, config);
+
+    const stripeProvider: Provider = {
+      provide: STRIPE_CLIENT,
+      useValue: stripe,
+    };
+
+    return {
+      module: StripeModule,
+      providers: [stripeProvider],
+      exports: [stripeProvider],
+      global: true,
+    };
+  }
+}
