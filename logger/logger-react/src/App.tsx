592bd83e24900f8d2ac13125eb45d483015269e7
Jun Guo <gjuoun@gmail.com>
2022-09-02 12:09:03 -0300

    winston is old

--- logger/logger-react/src/App.tsx
+++ logger/logger-react/src/App.tsx
@@ -1,31 +1,36 @@
 import React, { useEffect } from "react";
+import pino from 'pino'
 
-import winston, { createLogger, transports } from 'winston'
+// const  logger =pino({
+//   browser: {
+//     asObject: true
+//   }
+// })
+
+import * as winston from "winston";
 import BrowserConsole from 'winston-transport-browserconsole';
-
-
+ 
+const level = "debug";
 winston.configure({
-  transports: [
-      new BrowserConsole(
-          {
-              format: winston.format.simple(),
-              
-          },
-      ),
-      // Uncomment to compare with default Console transport
-      // new winston.transports.Console({
-      //     format: winston.format.simple(),
-      //     level,
-      // }),
-  ],
+    transports: [
+        new BrowserConsole(
+            {
+                format: winston.format.simple(),
+                level,
+            },
+        ),
+        // Uncomment to compare with default Console transport
+        // new winston.transports.Console({
+        //     format: winston.format.simple(),
+        //     level,
+        // }),
+    ],
 });
-
-export default logger
 
 export const App = () => {
 
   useEffect(()=> {
-    logger.info('App started')
+    winston.info('App started')
   })
 
   return <div>hello</div>;
